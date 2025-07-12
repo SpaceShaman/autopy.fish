85e5f548ca3addce507b6d8a2c6403ebee8d1e19
SpaceShaman <spaceshaman@tuta.io>
2025-07-12 16:38:36 +0200

    ♻️ (autopy.fish): refactor indentation and whitespace for consistency and readability

--- conf.d/autopy.fish
+++ conf.d/autopy.fish
@@ -8,7 +8,7 @@ function activate_venv --on-variable PWD
 
   if is_venv_active && is_old_venv_active $project_dir
     deactivate
-  end  
+  end
 
   if is_poetry_project $project_dir
     set venv_dir (command poetry show -v 2> /dev/null | grep 'Using virtualenv:' | cut -d ' ' -f 3)
@@ -16,15 +16,15 @@ function activate_venv --on-variable PWD
     set -l venv_dir_names env .env venv .venv
     set venv_dir ""
     for name in $venv_dir_names
-        if test -e "$project_dir/$name/bin/activate.fish"
-            set venv_dir "$project_dir/$name"
-            break
+      if test -e "$project_dir/$name/bin/activate.fish"
+        set venv_dir "$project_dir/$name"
+          break
         end
     end
   end
 
   if test -z "$venv_dir"
-      return
+    return
   end
 
   if is_venv_active && is_outside_venv $venv_dir
@@ -42,24 +42,23 @@ function activate_venv --on-variable PWD
 end
 
 function is_git_repo
-    command git rev-parse --show-toplevel &>/dev/null
+  command git rev-parse --show-toplevel &>/dev/null
 end
 
 function is_poetry_project -a dir
-    command -q poetry && test -e "$dir/pyproject.toml"
+  command -q poetry && test -e "$dir/pyproject.toml"
 end
 
 function is_venv_active
-    test -n "$VIRTUAL_ENV"
+  set python_bin (command which python)
+  test "$python_bin" = "$VIRTUAL_ENV/bin/python"
 end
 
 function is_outside_venv -a dir
-    test "$VIRTUAL_ENV" != "$dir"
+  test "$VIRTUAL_ENV" != "$dir"
 end
 
 function is_old_venv_active -a dir
-    test "$OLD_PROJECT_DIR" != "$dir"
+  test "$OLD_PROJECT_DIR" != "$dir"
 end
-
-activate_venv
 
